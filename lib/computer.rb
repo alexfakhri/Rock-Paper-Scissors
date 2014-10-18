3812517a7898a73827989f6ef3e8b2cd1fb0ca4c
alexfakhri <alexfakhri@me.com>
2014-10-18 18:55:55 +0100

    Created computer class for human player to play against

---
+++ lib/computer.rb
@@ -0,0 +1,15 @@
+class Computer
+
+	def gestures
+		[:rock, :scissors, :paprer]
+	end
+
+	def select_gesture
+		gestures.sample
+	end
+
+
+
+
+
+end
---
+++ spec/computer_spec.rb
@@ -0,0 +1,19 @@
+require 'computer'
+
+describe Computer do
+
+	let(:computer) { Computer.new }
+
+
+	it "should have a list of avaiblable gestures" do
+		expect(computer.gestures.count).to eq(3)
+	end
+
+	it "should be able to pick a random gesture" do
+		expect(computer.select_gesture).to be_a Symbol
+	end
+
+
+
+
+	end
